768b0d18bc05c228f78ca6a41e450dc64460017f
Srikanth Kavuri <ksrs9312@gmail.com>
2017-09-14 14:28:27 +0530

    Add files via upload
    Bag of words and LCS working properly, pending approval. Upload cos of MSIT proxy.

--- testing.c
+++ testing.c
@@ -7,41 +7,151 @@
 
 #include "testing.h"
 
+extern int mod;
+
+
+
 int main(int argc, char const *argv[]) {
   /* code */
 
   if(argc != 2) {
     fprintf(stderr, "Wrong number of args!\n");
-    fprintf(stderr, "Usage: ./testing <file name>\n");
+    fprintf(stderr, "Usage: ./testing <directory>\n");
     return 1;
   }
 
-  FILE *fp = fopen(argv[1], "r");
-  if(!fp) {
-    fprintf(stderr, "Couldn't open file: %s\n", argv[1]);
-    return 1;
+  char *temp,
+       *abs_path = realpath((char*)argv[1], NULL),
+       *str;
+
+  // printf("path: %s\n", (char*)argv[1]);
+  // printf("absolute path: %s\n", abs_path);
+
+  int num_files = 0, i = 0, j = 0;
+  char **file_names = get_all_files_in_dir((char*)argv[1], &num_files);
+  // char **strings = (char**)allocate((num_files) * sizeof(char*));
+
+  struct file_data *files = (struct file_data *)allocate(num_files * sizeof(struct file_data));
+
+  // Open the file, read its contents, degrammarify them, and arrange them
+  // into a single string.
+  for(i = 0; i < num_files; i++) {
+    str = (char*)allocate((1 + strlen(abs_path)) * sizeof(char));
+    strcpy(str, abs_path);
+
+    temp = (char*)allocate((2 + strlen(str) + strlen(*(file_names + i))) * sizeof(char));
+    strcpy(temp, strcat(strcat(str, "/"), *(file_names + i)));
+    // printf("\n%s is a text file!\n", temp);
+
+    (files + i)->f_name = (char*)allocate((1 + strlen(temp)) * sizeof(char));
+    strcpy((files + i)->f_name, temp);
+
+    (files + i)->string = degrammarify(get_string_from_file(temp));
+    // printf("file string: %s\n", (files + i)->string);
+    // printf("\nstrings: %s *** %s\n", (files + i)->string, degrammarify(get_string_from_file(temp)));
+
+    (files + i)->num_words = num_words((files + i)->string);
+    (files + i)->words = split_into_words(strdup((files + i)->string), (files + i)->num_words);
+    // (files + i)->hash = apply_hash_to_string(strdup((files + i)->string), (files + i)->num_words);
+    // printf("\n--------------strings: %s\n", (files + i)->string);
+
+    // required later.
+    (files + i)->hash = apply_hash_to_word_list((files + i)->words, (files + i)->num_words);
+
+    deallocate(temp);
+    deallocate(str);
+    // printf("Dealloacted!\n");
   }
 
-  char *str_one = (char*)allocate(1024),
-       *str_two = (char*)calloc(1, 1024 * sizeof(char)),
-       *temp;
+  // For each string, do bag of words, LCS, and fingerprinting
+  /*
+  printf("\n");
+  for(i = 0; i < num_files; i++) {
+    printf("File details:\n");
+    printf("File name: %s\n", (files + i)->f_name);
+    printf("String: %s\n", (files + i)->string);
 
-  fgets(str_one, 1024, fp);
-  fgets(str_two, 1024, fp);
+    // print_string((files + i)->string);
+    printf("Number of words: %d\n", (files + i)->num_words);
+    printf("Hash: ");
+    print_vector((files + i)->hash, mod);
+    printf("\n");
+    // printf("string for file %d: %s\n", i, *(strings + i));
+  }
+  */
+  // printf("\n\nword: %d, apple: %d, word: %d\n", apply_hash_to_word("word", mod),
+  //                                               apply_hash_to_word("apple", mod),
+  //                                               apply_hash_to_word("word", mod));
 
-  if((temp = strchr(str_one, '\n')) != NULL)
-    *temp = '\0';
+#if 0
+  float bag[num_files][num_files];
+  int j = 0;
+  for(i = 0; i < num_files; i++) {
+    for(j = 0; j < num_files; j++) {
+      bag[i][j] = get_dot_product((files + i)->hash, (files + j)->hash);
+    }
+  }
+#endif
 
-  printf("str one: %s, str two: %s\n\n\n", str_one, str_two);
+  printf("\n\nFYI.\nfile # -> file name\n");
+  for(i = 0; i < num_files; i++) {
+    printf("%d -> %s\n", i, (files + i)->f_name);
+  }
 
-  get_lcs_length(str_one, str_two);
+  float **bag = bag_driver(files, num_files);
 
-  printf("\n\nword: %d, apple: %d, word: %d\n", apply_hash_to_word("word", 10007),
-                                            apply_hash_to_word("apple", 10007),
-                                            apply_hash_to_word("word", 10007));
+  printf("\nBag of words:\n");
+  printf(" files ");
+  for(i = 0; i < num_files; i++)
+    printf("%7d", i);
+  printf("\n");
 
-  free(str_one);
-  free(str_two);
+
+  for(i = 0; i < num_files; i++) {
+    printf("% 4d    ", i);
+    for(j = 0; j < num_files; j++)
+      printf("%7.3f ", bag[i][j]);
+    printf("\n");
+  }
+
+#if 1
+  float **lcs = lcs_driver(files, num_files);
+
+  printf("\nLongest Common Subsequence:\n");
+  printf(" files ");
+  for(i = 0; i < num_files; i++)
+    printf("%7d", i);
+  printf("\n");
+
+
+  for(i = 0; i < num_files; i++) {
+    printf("% 4d    ", i);
+    for(j = 0; j < num_files; j++)
+      printf("%7.3f ", lcs[i][j]);
+    printf("\n");
+  }
+#endif
+
+  for(i = 0; i < num_files; i++) {
+    deallocate(*(bag + i));
+    deallocate((files + i)->f_name);
+    deallocate((files + i)->string);
+    deallocate((files + i)->hash);
+
+    // printf("Deallocating words\n");
+    // for(j = 0; j < ((files + i)->num_words); j++) {
+    //   printf("%s\n", (*(files+i)).words[j]);
+    //   deallocate((*(files+i)).words[j]);
+    // }
+    // printf("half done\n");
+    deallocate((files+i)->words);
+    // printf("totally done\n");
+  }
+  deallocate(bag);
+  deallocate(files);
+  // deallocate(strings);
+  // deallocate(str_one);
+  // deallocate(str_two);
 
   return 0;
 }
