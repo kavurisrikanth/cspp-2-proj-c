72996bf1beafe8cb4815da50d7fcacff838520a0
Srikanth Kavuri <ksrs9312@gmail.com>
2017-09-14 14:27:27 +0530

    Add files via upload
    Bag of words and LCS working properly, pending approval. Upload cos of MSIT proxy.

--- string_ops.c
+++ string_ops.c
@@ -4,6 +4,7 @@ String operations for the plagiarism project.
 
 #include "string_ops.h"
 #include "memory.h"
+#include <stdio.h>
 
 char* degrammarify(char *str) {
     // Removes all the punctuation from str, converts it to lowercase
@@ -27,6 +28,7 @@ char* degrammarify(char *str) {
            }
     }
 
+    deallocate(str);
     return new_str;
 }
 
@@ -34,9 +36,40 @@ char** split_into_words(char *str, int words) {
   /*
     Splits a string into an array of words
   */
-  char *temp,
-       **ans = (char**)calloc(1, words * sizeof(char*));
-  int i = 0;
+  // printf("\nreceived string: %s\n", str);
+  char **ans = (char**)calloc(1, words * sizeof(char*)),
+       c;
+  int i = 0, len = strlen(str), from = 0, j = 0;
+
+
+  for(i = 0; i <= len; i++) {
+    c = *(str + i);
+    // printf("c = %c\n", c);
+    if(c == '\0') {
+      // printf("string terminating char\n");
+      // printf("i = %d, j = %d, from = %d\n", i, j, from);
+      *(ans + j) = (char*)allocate((i - from + 1) * sizeof(char));
+      memcpy(*(ans + j), str + from, (i - from) * sizeof(char));
+      *(*(ans + j) + i - from) = '\0';
+      j++;
+    }
+
+    if(c == ' ') {
+      *(ans + j) = (char*)allocate((i - from) * sizeof(char));
+      memcpy(*(ans + j), str + from, (i - from) * sizeof(char));
+      *(*(ans + j) + i - from) = '\0';
+      from = i + 1;
+      j++;
+    }
+  }
+
+  // printf("String: %s\n", str);
+  // printf("Word list: \n");
+  // for(i = 0; i < words; i++)
+  //   printf(" ^%s^ ", *(ans + i));
+  // printf("\n");
+
+  #if 0
   temp = strtok(str, " \0");
   while(temp != NULL) {
     *(ans + i) = (char*)calloc(1, (1 + strlen(temp)) * sizeof(char));
@@ -44,7 +77,9 @@ char** split_into_words(char *str, int words) {
     i++;
     temp = strtok(NULL, " \0");
   }
+  #endif
 
+  deallocate(str);
   return ans;
 }
 
